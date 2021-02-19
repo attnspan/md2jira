84c682a43c9a11b04ad00bee79a79472e5110c55
Dan Racanelli <dan@decagames.com>
2021-02-20 00:37:20 +0100

    v0.2 working

--- convert_markdown_to_jira_tickets.sh
+++ convert_markdown_to_jira_tickets.sh
@@ -6,18 +6,37 @@ EPIC_ID=''
 STORY_ID=''
 
 SUMMARY_FOUND=0
+EPIC_FOUND=0
+STORY_FOUND=0
+SUBTASK_FOUND=0
 DESCRIPTION=''
+CMD=''
 
 # @see https://j.mp/3qAGH1q for WTF about the three slashes (plus spaces) below
+
+detect_issue() {
+    local LINE=${1}
+    local EPIC_REX="^#[[:space:]]"
+    local STORY_REX="^##[[:space:]]"
+    local SUBTASK_REX="^##[[:space:]]"
+
+    if [[ ${LINE} =~ ${EPIC_REX} ]]; then
+        EPIC_FOUND=1
+    elif [[ ${LINE} =~ ${STORY_REX} ]]; then
+        STORY_FOUND=1
+    elif [[ ${LINE} =~ ${SUBTASK_REX} ]]; then
+        SUBTASK_FOUND=1
+    fi
+}
 
 get_description() {
     EOD_REX="^---$"
     if [[ ${1} =~ ${EOD_REX} ]]; then
-        printf "${DESCRIPTION}\\\ "
+        #printf "YAY: ${DESCRIPTION}\\\ "
         SUMMARY_FOUND=0
         return 0
     else
-        [[ ! -z ${1} ]] && DESCRIPTION="${DESCRIPTION}\\\ ${1}"
+        DESCRIPTION="${DESCRIPTION}\\\\\ ${1}"
         return 1
     fi
 }
@@ -28,8 +47,22 @@ while read "LINE"; do
         get_description "${LINE}"
         RES=$?   
         if [[ ${RES} -eq 0 ]]; then
-            jira create --noedit -p DRT -o summary="GCore CDN" -o description="${DESCRIPTION}"
-            exit 0
+            CMD+=' -o description="'${DESCRIPTION}'"'
+            if [[ ${SUBTASK_FOUND} -eq 1 ]]; then
+                CMD+=" ${STORY_ID}"
+            fi
+            echo ${CMD}
+            RES=$(bash -c "${CMD}")
+            if [[ ${STORY_FOUND} -eq 1 ]]; then
+                STORY_ID=$(echo ${RES} | awk '{print $2}')
+                echo "STORY_ID: ${STORY_ID}"
+                STORY_FOUND=0
+            elif [[ ${SUBTASK_FOUND} ]]; then
+                SUBTASK_ID=$(echo ${RES} | awk '{print $2}')
+                echo "SUBTASK_ID: ${SUBTASK_ID}"
+                SUBTASK_FOUND=0
+            fi
+            SUMMARY_FOUND=0
         fi
     else
         DESCRIPTION=''
@@ -54,8 +87,10 @@ while read "LINE"; do
         echo "Story: ${LINE}"
         STORY=$(echo ${LINE} | sed -e 's@'${STORY_REX}'@@')
         SUMMARY_FOUND=1
+        STORY_FOUND=1
         if [[ DEBUG -eq 1 ]]; then
-            echo 'jira create --noedit -p DRT -o summary="'${STORY}'" -o "epiclink"="'${EPIC_ID}'"'
+            CMD='jira create --noedit -p DRT -o summary="'${STORY}'"'
+            [[ ! -z ${EPIC_ID} ]] && CMD="${CMD} -o \"epiclink\"=\"${EPIC_ID}\""
         else
             RES=$(jira create --noedit -p DRT -o summary="${STORY}" -o "epiclink"="${EPIC_ID}")
             STORY_ID=$(echo ${RES} | awk '{print $2}')
@@ -69,10 +104,11 @@ while read "LINE"; do
         echo "Subtask: ${LINE}"
         SUBTASK=$(echo ${LINE} | sed -e 's@'${SUBTASK_REX}'@@')
         SUMMARY_FOUND=1
+        SUBTASK_FOUND=1
         if [[ DEBUG -eq 1 ]]; then
-            echo 'jira subtask --noedit -p DRT -o summary="'${SUBTASK}'" ${STORY_ID}'
+            CMD='jira subtask --noedit -p DRT -o summary="'${SUBTASK}'"'
         else	
-            RES=$(jira subtask --noedit -p DRT -o summary="${SUBTASK}" ${STORY_ID})
+            RES=$(jira subtask --noedit -p DRT -o summary="${SUBTASK}")
             SUBTASK_ID=$(echo ${RES} | awk '{print $2}')
             echo "RES: ${RES}"
             echo "SUBTASK_ID: ${SUBTASK_ID}"

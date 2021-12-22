27e4917f6bf733a42a87641e90858d5f481f3b74
Dan Racanelli <dan@decagames.com>
2021-12-22 13:58:13 +0100

    WIP: Cacheing hashes of issues now

--- convert_markdown_to_jira_tickets.sh
+++ convert_markdown_to_jira_tickets.sh
@@ -13,7 +13,6 @@ SUBTASK_FOUND=0
 DESCRIPTION=''
 CMD=''
 
-# @see https://j.mp/3qAGH1q for WTF about the three slashes (plus spaces) below
 
 find_issue() {
     local SUMMARY=$(echo ${1} | tr ' ' '+')
@@ -26,7 +25,6 @@ find_issue() {
         KEY=$(echo ${KEY} | tr -d '"')
     fi
     local RES=$?
-    #echo "RES: ${RES}, TOTAL: ${TOTAL}, KEY: "$(echo ${KEY} | tr -d '"')
     echo ${TOTAL} ${KEY}
 }
 
@@ -78,17 +76,22 @@ prepare_subtask() {
 
 detect_issue() {
     local LINE=${1}
-    local EPIC_REX="^#[[:space:]]"
-    local STORY_REX="^##[[:space:]]"
-    local SUBTASK_REX="^###[[:space:]]"
+    EPIC_REX="^#[[:space:]]"
+    STORY_REX="^##[[:space:]]"
+    SUBTASK_REX="^###[[:space:]]"
 
     if [[ ${LINE} =~ ${EPIC_REX} ]]; then
         EPIC_FOUND=1
+        return 0
     elif [[ ${LINE} =~ ${STORY_REX} ]]; then
         STORY_FOUND=1
+        return 0
     elif [[ ${LINE} =~ ${SUBTASK_REX} ]]; then
         SUBTASK_FOUND=1
+        return 0
     fi
+
+    return 1
 }
 
 get_description() {
@@ -103,30 +106,83 @@ get_description() {
     fi
 }
 
+generate_issue_hash() {
+
+    local SUMMARY=${1}
+    local DESCRIPTION=${2}
+    local ISSUE_HASH=''
+
+    ISSUE_HASH=$(echo -n "${SUMMARY}:${DESCRIPTION}" | md5 | tr -d '\n')
+    echo -e ${ISSUE_HASH} 
+}
+
+check_issue_cache_hash() {
+    local ISSUE_KEY=${1}
+    local ISSUE_HASH=${2}
+
+    if [ "$(grep ${ISSUE_KEY} .md2jira_cache.tsv | awk '{print $NF}' 2>/dev/null)" = "${ISSUE_HASH}" ]; then
+        return 0
+    else
+        return 1 
+    fi
+
+}
+
+update_issue_cache() {
+    local ISSUE_KEY=${1}
+    local SUMMARY=${2}
+    local DESCRIPTION=${3}
+    local ISSUE_CACHE_FILE='./.md2jira_cache.tsv'
+    local TMP_ISSUE_CACHE_FILE=$(mktemp)
+
+    #ISSUE_HASH=$(echo -n "${SUMMARY}:${DESCRIPTION}" | md5 | tr -d '\n')
+    ISSUE_HASH=$(generate_issue_hash "${SUMMARY}" "${DESCRIPTION}")
+
+    [ ! -f .md2jira_cache.tsv ] && touch ${ISSUE_CACHE_FILE}
+    # If issue already exists
+    if check_issue_cache_hash ${ISSUE_KEY} ${ISSUE_HASH}; then
+        echo "Issue ${ISSUE_KEY}: ${SUMMARY} already cached and unchanged, skipping ..."
+    else
+        # Swap temp cache file with current
+        cat ${ISSUE_CACHE_FILE} | grep -v ${ISSUE_KEY} > ${TMP_ISSUE_CACHE_FILE}
+        echo -e "${ISSUE_KEY}\t\"${SUMMARY}\"\t${ISSUE_HASH}" >> ${TMP_ISSUE_CACHE_FILE}
+        mv ${TMP_ISSUE_CACHE_FILE} ${ISSUE_CACHE_FILE}
+    fi
+}
+
 while read "LINE"; do
 
-    # echo "LINE (${SUMMARY_FOUND}): ${LINE}"
-
     if [[ ${SUMMARY_FOUND} -eq 1 ]]; then
-        get_description "${LINE}"
-        RES=$?   
-        if [[ ${RES} -eq 0 ]]; then
+        if get_description "${LINE}"; then
+            # TODO: Remember what this regex does
             DESCRIPTION=$(echo "${DESCRIPTION}" | sed -e 's@^[\]*\ \([[:alpha:]]\)@\1@g')
 
             if [[ ${STORY_FOUND} -eq 1 ]]; then
 
                 read KEYS_FOUND KEY <<< $(find_issue "${STORY}")
-                
-                if [[ ${KEYS_FOUND} = 0 ]]; then
+
+                if [[ ${KEYS_FOUND} = 1 ]]; then
+                    # Check if DESCRIPTION has been updated
+                    ISSUE_HASH=$(generate_issue_hash "${STORY}" "${DESCRIPTION}")
+                    if check_issue_cache_hash ${KEY} ${ISSUE_HASH}; then
+                        echo "${KEY} hash MATCH ... ${ISSUE_HASH}"
+                    else
+                        echo "${KEY} hash MISMATCH ... ${ISSUE_HASH}"
+                        # TODO: START_HERE_NEXT
+                        # 1. Update JIRA issue with new description
+
+                        # 2. Update issue cache
+                        update_issue_cache ${KEY} "${STORY}" "${DESCRIPTION}"
+                    fi
+                else
+                     # TODO: Deal with next line assuming EPIC_ID exists
                     STORY_TMPFILE=$(prepare_story ${PROJECT} ${EPIC_ID} "${STORY}" "${DESCRIPTION}")
                     RES=$(bash -c "./test_cli.sh ${STORY_TMPFILE}")
                     STORY_ID=$(echo ${RES} | jq -r '.key')
                     echo "${RES}"
                     echo "STORY_ID: ${STORY_ID}"
-                else 
-                    echo "Story \"${KEY}: ${STORY}\" found, skipping ..."
                 fi
-
+                
                 STORY_FOUND=0
 
             elif [[ ${SUBTASK_FOUND} ]]; then
@@ -141,6 +197,8 @@ while read "LINE"; do
                     echo "SUBTASK_ID: ${SUBTASK_ID}"
                 else 
                     echo "Sub-task \"${KEY}: ${SUBTASK}\" found, skipping ..."
+                    # Write md5 of ticket to cache
+                    update_issue_cache ${KEY} "${SUBTASK}" "${DESCRIPTION}"
                 fi
 
                 SUBTASK_FOUND=0
@@ -152,25 +210,30 @@ while read "LINE"; do
         DESCRIPTION=''
     fi
 
-    EPIC_REX="^#[[:space:]]"
-    if [[ ${LINE} =~ ${EPIC_REX} ]]; then
-        EPIC=$(echo ${LINE} | sed -e 's@'${EPIC_REX}'@@')
-        echo "Epic: ${EPIC}"
+    if detect_issue "${LINE}"; then
+        if [[ ${EPIC_FOUND} -eq 1 ]]; then
+            EPIC=$(echo ${LINE} | sed -e 's@'${EPIC_REX}'@@')
+            echo "Epic: ${EPIC}"
+            TMP_DESCRIPTION="fooey_desc"
 
-        read KEYS_FOUND KEY <<< $(find_issue "${EPIC}")
+            read KEYS_FOUND KEY <<< $(find_issue "${EPIC}")
 
-        if [[ ${KEYS_FOUND} = 0 ]]; then
-            echo "EPIC NOT FOUND"
-            EPIC_TMPFILE=$(prepare_epic ${PROJECT} "${EPIC}" "fooey_desc")
-            echo "EPIC_TMPFILE: ${EPIC_TMPFILE}"
-            RES=$(bash -c "./test_cli.sh ${EPIC_TMPFILE}")
-            EPIC_ID=$(echo ${RES} | jq -r '.key')
-            echo "RES: ${RES}"
-            echo "EPIC_ID: ${EPIC_ID}"
-        else 
-            echo "EPIC \"${KEY}: ${EPIC}\" FOUND"
+            if [[ ${KEYS_FOUND} = 0 ]]; then
+                echo "EPIC NOT FOUND"
+                EPIC_TMPFILE=$(prepare_epic ${PROJECT} "${EPIC}" "${TMP_DESCRIPTION}")
+                echo "EPIC_TMPFILE: ${EPIC_TMPFILE}"
+                RES=$(bash -c "./test_cli.sh ${EPIC_TMPFILE}")
+                EPIC_ID=$(echo ${RES} | jq -r '.key')
+                echo "RES: ${RES}"
+                echo "EPIC_ID: ${EPIC_ID}"
+            else 
+                echo "Epic \"${KEY}: ${EPIC}\" found, skipping ..."
+                # Write md5 of ticket to cache
+                update_issue_cache ${KEY} "${EPIC}" "${TMP_DESCRIPTION}"
+            fi
+
+            EPIC_FOUND=0
         fi
-        
     fi
 
     STORY_REX="^##[[:space:]]"
